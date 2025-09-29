fa0a2560437bc94f7b3a1dde22a36c502f18c346
Landon Mendoza <landonmendoza@outlook.com>
2025-09-29 12:17:56 -0700

    Resolved Contexts

--- Project5/Project5/src/CircularLinkedList.cpp
+++ Project5/Project5/src/CircularLinkedList.cpp
@@ -60,29 +60,38 @@ CircularLinkedList::ListIter& CircularLinkedList::ListIter::operator=(const List
 
 void CircularLinkedList::PushBack(const value_type& p_NewElement)
 {
-	if (!m_HeadPtr)
+	if (!m_TailPtr)
 	{
 		m_HeadPtr = new Node();
 		m_HeadPtr->GetData() = p_NewElement;
+		m_TailPtr = m_HeadPtr;
 		m_NumOfElements++;
 		return;
 	}
 
-	Node* ptr = m_HeadPtr;
+	Node* oldTail = m_TailPtr;
 
-	while (ptr->HasNext())
-		ptr = ptr->GetNext();
 
-	ptr->GetNext() = new Node();
-	ptr->GetNext()->GetData() = p_NewElement;
+	m_TailPtr = new Node();
+	m_TailPtr->GetData() = p_NewElement;
 
-	m_TailPtr = ptr->GetNext();
+	oldTail->GetNext() = m_TailPtr;
+
+	m_TailPtr->GetNext() = m_HeadPtr;
 	m_NumOfElements++;
 }
 
 size_t CircularLinkedList::GetSize() const
 {
 	return m_NumOfElements;
+}
+
+void CircularLinkedList::Insert(size_t p_Index) const
+{
+}
+
+void CircularLinkedList::DeleteAt(size_t p_Index) const
+{
 }
 
 CircularLinkedList::iterator CircularLinkedList::begin()
@@ -116,7 +125,22 @@ bool CircularLinkedList::Node::HasNext() const
 	return m_Next != nullptr;
 }
 
+CircularLinkedList::Node*& CircularLinkedList::Node::GetPrevious()
+{
+	return m_Prev;
+}
+
+bool CircularLinkedList::Node::HasPrevious() const
+{
+	return m_Prev != nullptr;
+}
+
 CircularLinkedList::~CircularLinkedList()
+{
+	Clear();
+}
+
+void CircularLinkedList::Clear()
 {
 	Node* ptr = m_HeadPtr;
 
@@ -129,10 +153,10 @@ CircularLinkedList::~CircularLinkedList()
 		ptr = ptr->GetNext();
 		delete temp;
 	}
-}
 
-void CircularLinkedList::Clear() const
-{
+	m_HeadPtr = nullptr;
+	m_TailPtr = nullptr;
+	m_NumOfElements = 0;
 }
 
 void CircularLinkedList::Resize(size_t p_NewSize)
@@ -160,11 +184,19 @@ void CircularLinkedList::PushFront(const value_type& p_NewElement)
 	m_HeadPtr->GetData() = p_NewElement;
 
 	m_HeadPtr->GetNext() = oldHead;
+
+	oldHead->GetPrevious() = m_HeadPtr;
+
+	m_TailPtr->GetNext() = m_HeadPtr;
+
+	m_HeadPtr->GetPrevious() = m_TailPtr;
 	m_NumOfElements++;
 }
 
 void CircularLinkedList::PopFront()
 {
+	if (!m_TailPtr)
+		throw E_NullList();
 }
 
 CircularLinkedList::value_type& CircularLinkedList::Front()
@@ -174,6 +206,8 @@ CircularLinkedList::value_type& CircularLinkedList::Front()
 
 void CircularLinkedList::PopBack()
 {
+	if (!m_TailPtr)
+		throw E_NullList();
 }
 
 CircularLinkedList::value_type& CircularLinkedList::operator [](size_t p_Index)
@@ -222,4 +256,59 @@ std::string CircularLinkedList::E_NullList::GetExceptionName() const
 std::string CircularLinkedList::E_NullList::GetExceptionMessage() const
 {
 	return "The List is Empty!";
+}
+
+CircularLinkedList::ListReverseIter::ListReverseIter(CircularLinkedList& p_CircularLinkedList, Node* p_CurrentNode, size_t p_Increment)
+	: m_CircularLinkedList(p_CircularLinkedList),
+	m_CurrentNode(p_CurrentNode),
+	m_Increment(p_Increment)
+{}
+
+CircularLinkedList::ListReverseIter& CircularLinkedList::ListReverseIter::operator++()
+{
+	if (!m_CurrentNode->GetNext())
+	{
+		*this = m_CircularLinkedList.rend();
+		return *this;
+	}
+
+	m_CurrentNode = m_CurrentNode->GetNext();
+	m_Increment++;
+
+	return *this;
+}
+
+CircularLinkedList::ListReverseIter CircularLinkedList::ListReverseIter::operator++(int)
+{
+	auto temp = *this;
+
+	if (!m_CurrentNode->HasNext())
+	{
+		*this = m_CircularLinkedList.rend();
+		return *this;
+	}
+
+	m_CurrentNode = m_CurrentNode->GetNext();
+	m_Increment++;
+
+	return temp;
+}
+
+bool CircularLinkedList::ListReverseIter::operator!=(const ListReverseIter& p_Other) const
+{
+	return this->m_Increment != p_Other.m_Increment;
+}
+
+CircularLinkedList::value_type& CircularLinkedList::ListReverseIter::operator*()
+{
+	return m_CurrentNode->GetData();
+}
+
+CircularLinkedList::ListReverseIter& CircularLinkedList::ListReverseIter::operator=(const ListReverseIter& p_Other)
+{
+	this->m_Increment = p_Other.m_Increment;
+	this->m_CircularLinkedList = p_Other.m_CircularLinkedList;
+	this->m_CurrentNode = p_Other.m_CurrentNode;
+
+	return *this;
 }
